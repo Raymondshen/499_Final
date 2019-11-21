6498d0d14abdd8ce407aca23740329893d21e4dd
Hamilton <hamdiggy@gmail.com>
2019-11-21 14:58:49 -0800

    Module 12 lesson

--- alpha/src/pages/home/index.js
+++ alpha/src/pages/home/index.js
@@ -1,4 +1,4 @@
-import React, {useReducer} from 'react';
+import React, {useReducer,useState,useEffect} from 'react';
 
 import {
 	Switch,
@@ -15,10 +15,16 @@ const HomePage = () => {
 
 	// A reducer is a handy way of creating app data that is subscribed but somewhat custom
 	// These will reset if you ever navigate away from the Learn pages
-	let [fonts,setFont] = useReducer(
-		(s,a) => ({...s,...a}),
-		{first:'Arial',second:'serif'}
-	);
+	// let [fonts,setFont] = useReducer(
+	// 	(s,a) => ({...s,...a}),
+	// 	{first:'Arial',second:'serif'}
+	// );
+
+	let [font1,setFont1] = useState('Arial');
+	let [font2,setFont2] = useState('Times New Roman');
+	let [num,setNum] = useState(0);
+
+
 
 	return(
 		<section className="container max-xs-s p-xs-txl">
@@ -29,8 +35,31 @@ const HomePage = () => {
 			</div>
 			<div className="container">
 				<Switch>
-					<Route exact path={path}>
+					<Route exact path={`${path}`}>
 						<PairFont path={path}/>
+					</Route>
+					<Route path={`${path}/1`}>
+						<PairFont1
+							path={path}
+							font1={font1}
+							setFont1={setFont1}
+							/>
+					</Route>
+					<Route path={`${path}/2`}>
+						<PairFont2
+							path={path}
+							font2={font2}
+							setFont2={setFont2}
+							/>
+					</Route>
+					<Route path={`${path}/3`}>
+						<PairFont3
+							path={path}
+							font2={font2}
+							font1={font1}
+							num={num}
+							setNum={setNum}
+							/>
 					</Route>
 				</Switch>
 			</div>
@@ -41,6 +70,7 @@ const HomePage = () => {
 //Here is the start pairing content.
 //If this is the flow we want to go for. The flow is accesable from the home screen. 
 const PairFont = (props) => {
+	console.log(props)
 	return (
 		<section>
 			<div>
@@ -50,10 +80,48 @@ const PairFont = (props) => {
 
 
 			<div>
-				<div className="">
-					<div className=""/>
-					<Link className="" to={`${props.path}/1`}>Start!</Link>
-				</div>
+				<Link className="" to={`${props.path}/1`}>Start!</Link>
+			</div>
+		</section>
+	);
+}
+
+
+const PairFont1 = ({path,font1,setFont1}) => {
+	const updateFont = (e) => {
+		e.preventDefault();
+		setFont1(e.target.value);
+	}
+	return (
+		<section>
+			{font1}
+			<input value={font1} onChange={updateFont} />
+			<div><Link to={`${path}/2`}>Next</Link></div>
+		</section>
+	);
+}
+
+
+const PairFont2 = ({path,font2,setFont2}) => {
+	return (
+		<section>
+			{font2}
+			<div><Link to={`${path}/3`}>Next</Link></div>
+		</section>
+	);
+}
+const PairFont3 = (props) => {
+	const doNum = e => {
+		e.preventDefault();
+		props.setNum(e.target.value)
+	}
+	return (
+		<section>
+			<div>{props.font1}</div>
+			<div>{props.font2}</div>
+			<div>
+				<div>{props.num}</div>
+				<input type="range" value={props.num} min="0" max="20" onChange={doNum} />
 			</div>
 		</section>
 	);
