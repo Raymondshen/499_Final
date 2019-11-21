aab5fb4a8144e28466bbedbdad8c8d7cbbcfb461
Gustav <gustav.bodin1@gmail.com>
2019-11-20 18:05:54 -0800

    Folders

--- alpha/src/App.js
+++ alpha/src/App.js
@@ -2,11 +2,21 @@ import React from 'react';
 import logo from './assets/images/logo.svg';
 import './styles/App.css';
 
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Switch>
+        // <Route exact path="/" component={MainPage}/>
+        // <Route exact path="/" component={MainPage}/>
+      </Switch>
+    </Router>
   );
 }
 
