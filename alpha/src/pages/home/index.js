d44eca2a8423574e0c00587614af3573256b21a2
Gustav <gustav.bodin1@gmail.com>
2019-11-20 18:11:06 -0800

    Added style library

--- alpha/src/pages/home/index.js
+++ alpha/src/pages/home/index.js
@@ -1,1 +1,26 @@
 import React from 'react';
+
+const HomePage = () => {
+	return(
+		<section>
+			<div className=""><h1>[Font pairing app name]</h1></div>
+					<div className="container">
+			<div className="card pad">
+				<p>This page will be a short introduction to some basic, and even advanced topics for creating a React App from scratch. Open the code and step through the application part by part, as you analyse the inner workings.</p>
+
+				<h3>Relevant Links</h3>
+				<ul>
+					<li>Add React to a Page: <a href="https://reactjs.org/docs/add-react-to-a-website.html">https://reactjs.org/docs/add-react-to-a-website.html</a></li>
+					<li>Hooks: <a href="https://reactjs.org/docs/hooks-reference.html">https://reactjs.org/docs/hooks-reference.html</a></li>
+					<li>Hooks Examples: <a href="https://daveceddia.com/useeffect-hook-examples/">https://daveceddia.com/useeffect-hook-examples/</a></li>
+					<li>React Router: <a href="https://reacttraining.com/react-router/web/guides/quick-start">https://reacttraining.com/react-router/web/guides/quick-start</a></li>
+					<li>Deployment: <a href="https://create-react-app.dev/docs/deployment/#serving-apps-with-client-side-routing">https://create-react-app.dev/docs/deployment/#serving-apps-with-client-side-routing</a></li>
+					<li>Reducer Pattern: <a href="https://kentcdodds.com/blog/the-state-reducer-pattern-with-react-hooks">https://kentcdodds.com/blog/the-state-reducer-pattern-with-react-hooks</a></li>
+				</ul>
+			</div>
+		</div>
+		</section>
+		);
+} 
+
+export default HomePage;
