1dc9b6bc0d0797aad9ed59f68837655b4fe48f51
Gustav <gustav.bodin1@gmail.com>
2019-12-02 00:08:13 -0800

    Updating flow

--- alpha/src/pages/home/components.js
+++ alpha/src/pages/home/components.js
@@ -3,26 +3,33 @@ import React from 'react';
 //Question for pairing dock. How does one change the unique h1 for the user and not the whole document.
 
 //Components that's global
-const PairingDoc = ({fontStyle, fontName}) => {
+
+
+//Pairing doc, pass font style as a property as well
+const PairingDoc = ({fontStyle, fontSize, fontName}) => {
     return ( <>
       <section className="grid p-xs-m">
-        <div className="userInputH1 col--12">An Adaptable Foundation</div>
+        <h1 style={{fontFamily:fontStyle}} className="col--12">An Adaptable Foundation</h1>
       </section>
       <section className="grid p-xs-m p-xs-hs">
         <div className="col--6">
-          <div className="userInputH5">Headline 5</div>
-          <div className="userInputP">Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere, whose upper half.</div>
+          <h5 style={{fontFamily:fontStyle}}>Headline 5</h5>
+          <p style={{fontFamily:fontStyle}}>Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere, whose upper half.</p>
         </div>
         <div className="col--6 offset--7 p-xs-hs">
-        <div className="userInputH3">Headline 3</div>
-        <div className="userInputH4">Headline 4</div>
-          <div className="userInputP">
+        <h3 style={{fontFamily:fontStyle}}>Headline 3</h3>
+        <h4 style={{fontFamily:fontStyle}}>Headline 4</h4>
+          <p style={{fontFamily:fontStyle}}>
             By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere, whose upper half was.
-          </div>
+          </p>
         </div>
       </section>
       <section className="p-xs-hs">
-        <TypeHierchy style={fontStyle} text={fontName}/>
+        <TypeHierchy 
+          fontStyle={fontStyle} 
+          fontSize={fontSize} 
+          text={fontName}
+          />
       </section>
     </>
     );
@@ -43,34 +50,17 @@ const ClickList = ({data,callback}) => {
 }
 
 //Components that's local
-const TypeHierchy = ({fontStyle, text}) => {
+const TypeHierchy = ({fontStyle, fontSize, text}) => {
   return (
     <section>
-      <div className="userInputH1 m-xs-bs" style={{fontFamily:fontStyle}}>H1 / {text}</div>
-      <div className="userInputH2 m-xs-bs" style={{fontFamily:fontStyle}}>H2 / {text}</div>
-      <div className="userInputH3 m-xs-bs" style={{fontFamily:fontStyle}}>H3 / {text}</div>
-      <div className="userInputH4 m-xs-bs" style={{fontFamily:fontStyle}}>H4 / {text}</div>
-      <div className="userInputH5 m-xs-bs" style={{fontFamily:fontStyle}}>H5 / {text}</div>
-      <div className="userInputP m-xs-bs" style={{fontFamily:fontStyle}}>p / {text}</div>
+      <h1 className="userInputH1 m-xs-bs" style={{fontFamily:fontStyle, fontSize:fontSize}}>H1 / {text}</h1>
+      <h2 className="userInputH2 m-xs-bs" style={{fontFamily:fontStyle}}>H2 / {text}</h2>
+      <h3 className="userInputH3 m-xs-bs" style={{fontFamily:fontStyle}}>H3 / {text}</h3>
+      <h4 className="userInputH4 m-xs-bs" style={{fontFamily:fontStyle}}>H4 / {text}</h4>
+      <h5 className="userInputH5 m-xs-bs" style={{fontFamily:fontStyle}}>H5 / {text}</h5>
+      <p className="userInputP m-xs-bs" style={{fontFamily:fontStyle}}>p / {text}</p>
     </section>
   );
 }
 
-const Slider = () => {
-  return (
-    <section>
-    </section>
-  );
-}
-
-const FontCard = () => {
-  return (
-    <section>
-      <h4>
-        Aa
-      </h4>
-    </section>
-  );
-}
-
-export {PairingDoc, ClickList, Slider, FontCard};
+export {PairingDoc, ClickList};
--- alpha/src/pages/home/index.js
+++ alpha/src/pages/home/index.js
@@ -20,8 +20,9 @@ const HomePage = () => {
 	// These will reset if you ever navigate away from the Learn pages
 	let [fonts,setFont] = useReducer(
 		(s,a) => ({...s,...a}),
-		{first:'Arial',second:'serif'}
+		{first:'Arial', second:'serif'}
 	);
+	let [num,setNum] = useState(14);
 
 	return(
 		<article className="container max-xs-s p-xs-ts">
@@ -37,7 +38,16 @@ const HomePage = () => {
 						<PairFont 
 							path={path}
 							fonts={fonts}
+							num={num}
 							setFont={setFont}
+						/>
+					</Route>
+					<Route path={`${path}/1`}>
+						<SetSize 
+							path={path}
+							fonts={fonts}
+							num={num}
+							setNum={setNum}
 						/>
 					</Route>
 				</Switch>
@@ -50,16 +60,8 @@ const HomePage = () => {
 //If this is the flow we want to go for. The flow is accesable from the home screen. 
 
 const PairFont = ({path,fonts,setFont}) => {
+	//Here insert jason file with all of our fonts.
 	let fontlist = ['Arial', 'Arial Black', 'Tahoma', 'Verdana', 'Sans-Serif', 'Open-Sans'];
-	
-	const handleSubmit = (e) => {
-		e.preventDefault();
-	}
-
-	const handleChange = (e) => {
-		e.preventDefault();
-		setFont({first:e.target.value});
-	}
 
 	const handleInput = (e) => {
 		e.preventDefault();
@@ -71,13 +73,45 @@ const PairFont = ({path,fonts,setFont}) => {
 			<div className="col--6">
 				<div className="p-xs-bs"><Link to={`${path}/1`}>Next</Link></div>
 				<input className="font-input m-xs-bm" type="text" value={fonts.first} onChange={handleInput} />
-				<ClickList data={fontlist} callback={setFont} />
+				<ClickList 
+					data={fontlist} 
+					callback={setFont}
+					/>
 			</div>
 			<div className="col--6 offset--7 pairing-wrapper">
-				<PairingDoc fontStyle={fonts.first} fontName={fonts.first}/>
+				<PairingDoc 
+					fontSize={72} 
+					fontStyle={fonts.first} 
+					fontName={fonts.first}
+					/>
 			</div>
 		</section>
 	);
 }
+
+const SetSize = ({path,fonts,num,setNum}) => {
+
+	const doNum = e => {
+		e.preventDefault();
+		setNum(e.target.value)
+	}
+
+	return (
+		<section className="grid">
+			<div className="col--6">
+				<div className="p-xs-bs"><Link to={`${path}/2`}>Next</Link></div>
+				<input type="range" value={num} min="12" max="72" id="font-size" onChange={doNum}/>
+			</div>
+			<div className="col--6 offset--7 pairing-wrapper">
+				<PairingDoc 
+					fontSize={num} 
+					fontStyle={fonts.first} 
+					fontName={fonts.first}
+					/>
+			</div>
+		</section>
+	);
+}
+
 
 export default HomePage;
