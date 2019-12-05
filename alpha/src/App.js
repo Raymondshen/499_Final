3d9863f09b9f9ae940d686e8fbb0695cfb9f57e7
Dino Andrighetto <dinoghetto@gmail.com>
2019-12-05 00:58:41 -0800

    updated files

--- alpha/src/App.js
+++ alpha/src/App.js
@@ -1,25 +1,44 @@
+//Hello!
+//Suggested workfolw. Open a new terminal tab for node running react, sass compile and watch for style folder, and git for alpha folder. 
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import './styles/style.min.css';
+
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+//Here goes the components that we're importing to the main app.
+//The main components are the ones that will be displayed at all times. 
+//Etc. the nav, the footer. The components that will be in the blocks that make up the pages can be imported directlly into the pages and then the pages into the main app.
+import Nav from "./components/nav";
+
+//Here goes the pages that we're importing to the main app.
+//The pages are the contet that will be exchanged in the app.
+import HomePage from "./pages/home";
+import PairPage from "./pages/fontPairing";
+import AboutPage from "./pages/about";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav projectTitle="Pairing fonts">
+          <Link to="/">Home</Link>
+          <Link to="/pairfonts">How to pairfonts</Link>
+          <Link to="/about">About project</Link>
+      </Nav>
+      <main>
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+          <Route path="/pairfonts" component={PairPage}/>
+          <Route path="/about" component={AboutPage}/>
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
---
+++ alpha/src/pages/home/index.js
@@ -0,0 +1,62 @@
+import React, {useReducer} from 'react';
+
+import {
+	Switch,
+	Route,
+	Link,
+	useRouteMatch
+} from 'react-router-dom';
+//Haven't started styling the app. Just setting up the routing. Have applied some easy classes that I had in my library to make it easy to look at.
+//https://mannenpag.github.io/sass-library/
+
+//Here is home content. 
+const HomePage = () => {
+	let {path} = useRouteMatch();
+
+	// A reducer is a handy way of creating app data that is subscribed but somewhat custom
+	// These will reset if you ever navigate away from the Learn pages
+	let [fonts,setFont] = useReducer(
+		(s,a) => ({...s,...a}),
+		{first:'Arial',second:'serif'}
+	);
+
+	return(
+		<section className="container max-xs-s p-xs-txl">
+			<div className="container">
+				<h4>Welcome to our font pairing app</h4>
+				<p>This is our app for pairing fonts.</p>
+				<p className="max-xs-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+			<div className="container">
+				<Switch>
+					<Route exact path={path}>
+						<PairFont path={path}/>
+					</Route>
+				</Switch>
+			</div>
+		</section>
+		);
+}
+
+//Here is the start pairing content.
+//If this is the flow we want to go for. The flow is accesable from the home screen. 
+const PairFont = (props) => {
+	return (
+		<section>
+			<div>
+				<h3>Here is info about the pairing process</h3>
+				<p className="max-xs-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+
+
+			<div>
+				<div className="">
+					<div className=""/>
+					<Link className="" to={`${props.path}/1`}>Start!</Link>
+				</div>
+			</div>
+		</section>
+	);
+}
+
+export default HomePage;
