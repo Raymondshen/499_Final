f2042c06fc917888273f7de93ba5a6b8199ef860
Hamilton <hamdiggy@gmail.com>
2019-12-05 15:23:55 -0800

    states and effects

--- alpha/src/pages/home/index.js
+++ alpha/src/pages/home/index.js
@@ -9,19 +9,45 @@ import {
 //Haven't started styling the app. Just setting up the routing. Have applied some easy classes that I had in my library to make it easy to look at.
 //https://mannenpag.github.io/sass-library/
 
-import {PairingDoc} from "./components";
-import {ClickList} from "./components";
+import {PairingDoc,ClickList,FontRange} from "./components";
+
+
 
 //Here is home content. 
-const HomePage = () => {
+const HomePage = ({fonts}) => {
 	let {path} = useRouteMatch();
 
-	let [fonts,setFont] = useReducer(
+	let [fontFamilies,setFontFamilies] = useReducer(
 		(s,a) => ({...s,...a}),
-		{first:'Arial', second:'serif'}
+		{first:{}, second:{}}
+	);
+	let [fontSizes,setFontSizes] = useReducer(
+		(s,a) => {
+			s[+a.i]={...s[+a.i],...a.v};
+			return [...s];
+		},
+		[
+			{size:72,min:10,max:72,name:'H1'},
+			{size:56,min:10,max:72,name:'H2'},
+			{size:43,min:10,max:72,name:'H3'},
+			{size:16,min:10,max:72,name:'H4'},
+			{size:14,min:10,max:72,name:'H5'},
+			{size:10,min:10,max:72,name:'P'}
+		]
 	);
 
-	let [num,setNum] = useState(14);
+	useEffect(()=>{
+		if(!fonts.length) return;
+		let r1 = Math.floor(Math.random() * fonts.length);
+		let r2 = Math.floor(Math.random() * fonts.filter((o,i)=>i!=r1).length);
+		console.log(r1,r2,fonts[r1],fonts[r2])
+		setFontFamilies({first:fonts[r1]})
+		setFontFamilies({second:fonts[r2]})
+	},[fonts]);
+
+
+
+	//let [fontSize,setFontSize] = useState(14);
 
 	return(
 		<article className=" container p-xs-ts">
@@ -36,17 +62,21 @@ const HomePage = () => {
 					<Route exact path={`${path}`}>
 						<PairFont 
 							path={path}
-							fonts={fonts}
-							num={num}
-							setFont={setFont}
+
+							fontlist={fonts}
+
+							setFontFamilies={setFontFamilies}
+							fontFamilies={fontFamilies}
+							fontSizes={fontSizes}
 						/>
 					</Route>
 					<Route path={`${path}/1`}>
 						<SetSize 
 							path={path}
-							fonts={fonts}
-							num={num}
-							setNum={setNum}
+
+							setFontSizes={setFontSizes}
+							fontFamilies={fontFamilies}
+							fontSizes={fontSizes}
 						/>
 					</Route>
 				</Switch>
@@ -58,65 +88,75 @@ const HomePage = () => {
 //Here is the start pairing content.
 //If this is the flow we want to go for. The flow is accesable from the home screen. 
 
-const PairFont = ({path,fonts,setFont}) => {
+const PairFont = ({fontlist,path,setFontFamilies,fontFamilies,fontSizes}) => {
 	//Here insert jason file with all of our fonts.
-	let fontlist = ['Arial', 'Arial Black', 'Tahoma', 'Verdana', 'Sans-Serif', 'Times New Roman', 'Cambria', 'Georgia', 'Serif'];
+	console.log(fontlist)
+	// fontlist = ['Arial', 'Arial Black', 'Tahoma', 'Verdana', 'Sans-Serif', 'Times New Roman', 'Cambria', 'Georgia', 'Serif'];
+
 
 	const changeFontOne = (e) => {
 		e.preventDefault();
-		setFont({first:e.target.value});
+		setFontFamilies({first:e.target.value});
 	}
 
 	const changeFontTwo = (e) => {
 		e.preventDefault();
-		setFont({second:e.target.value});
+		setFontFamilies({second:e.target.value});
 	}
 	return (
 		<section className="grid">
-			<div className="bg-dark col--6">
+			<div className="col--6">
 				<div className="p-xs-bs"><Link to={`${path}/1`}>Next</Link></div>
-				<section className="grid">
-					<input className="font-input m-xs-bm col--6" type="text" value={fonts.first} onChange={changeFontOne} />
-					<input className="font-input p-xs-s m-xs-bm offset--7 col--6" type="text" value={fonts.second} onChange={changeFontTwo} />
-				</section>
+				{/* <section className="grid">
+					<input className="font-input m-xs-bm col--6" type="text" value={fontFamilies.first.name} onChange={changeFontOne} />
+					<input className="font-input p-xs-s m-xs-bm offset--7 col--6" type="text" value={fontFamilies.second.name} onChange={changeFontTwo} />
+				</section> */}
+				<div className="bg-dark">
 				<ClickList 
 					data={fontlist} 
-					callback={setFont}
+					families={fontFamilies}
+					callback={setFontFamilies}
 					/>
+				</div>
 			</div>
 			<div className="col--6 offset--7 pairing-wrapper">
 				<PairingDoc 
-					fontSize={72} 
-					fontStyleTwo={fonts.second} 
-					fontStyleOne={fonts.first} 
-					fontNameOne={fonts.first}
-					fontNameTwo={fonts.second}
+					fontSizes={fontSizes} 
+					fontFamilies={fontFamilies} 
 					/>
 			</div>
 		</section>
 	);
 }
 
-const SetSize = ({path,fonts,num,setNum}) => {
+const SetSize = ({path,setFontSizes,fontSizes,fontFamilies}) => {
 
-	const doNum = e => {
+	const setSize = e => {
 		e.preventDefault();
-		setNum(e.target.value)
+		setFontSizes({
+			i:e.target.dataset.id,
+			v:{size:e.target.value}
+		});
 	}
+
+	let r = Math.floor(Math.random() * fontSizes.length);
 
 	return (
 		<section className="grid">
 			<div className="col--6">
 				<div className="p-xs-bs"><Link to={`${path}/2`}>Next</Link></div>
-				<input type="range" value={num} min="12" max="72" id="font-size" onChange={doNum}/>
+				{ fontSizes.map((o,i)=>(
+					<FontRange
+						key={i}
+						id={i}
+						size={fontSizes}
+						setSize={setSize} />
+				))}
 			</div>
 			<div className="col--6 offset--7 pairing-wrapper">
 				<PairingDoc 
-					fontSize={num} 
-					fontStyleTwo={fonts.second} 
-					fontStyleOne={fonts.first} 
-					fontNameOne={fonts.first}
-					fontNameTwo={fonts.second}
+					fontFamilies={fontFamilies}
+					fontSizes={fontSizes}
 					/>
 			</div>
 		</section>
