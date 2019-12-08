82a2886e70f9fc73c78455606d5695835fdad910
Raymond Shen <Raymonds.shenz@gmail.com>
2019-12-07 19:23:42 -0800

    Minor Changes

--- alpha/src/pages/home/index.js
+++ alpha/src/pages/home/index.js
@@ -1,4 +1,4 @@
-import React, {useReducer,useState,useEffect} from 'react';
+import React, { useReducer, useState, useEffect } from 'react';
 
 import {
 	Switch,
@@ -9,32 +9,32 @@ import {
 //Haven't started styling the app. Just setting up the routing. Have applied some easy classes that I had in my library to make it easy to look at.
 //https://mannenpag.github.io/sass-library/
 
-import {PairingDoc} from "./components";
-import {ClickList} from "./components";
+import { PairingDoc } from "./components";
+import { ClickList } from "./components";
 
 //Here is home content. 
 const HomePage = () => {
-	let {path} = useRouteMatch();
+	let { path } = useRouteMatch();
 
-	let [fonts,setFont] = useReducer(
-		(s,a) => ({...s,...a}),
-		{first:'Arial', second:'serif'}
+	let [fonts, setFont] = useReducer(
+		(s, a) => ({ ...s, ...a }),
+		{ first: 'Arial', second: 'serif' }
 	);
 
-	let [num,setNum] = useState(14);
+	let [num, setNum] = useState(14);
 
-	return(
-		<article className=" container p-xs-ts">
-			<section className="grid">
+	return (
+		<article className=" container">
+			{/* <section className="grid">
 				<h4 className="col--4">Welcome to our font pairing app</h4>
 			</section>
 			<section className="grid">
 				<p className="col--4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-			</section>
+			</section> */}
 			<section>
 				<Switch>
 					<Route exact path={`${path}`}>
-						<PairFont 
+						<PairFont
 							path={path}
 							fonts={fonts}
 							num={num}
@@ -42,7 +42,7 @@ const HomePage = () => {
 						/>
 					</Route>
 					<Route path={`${path}/1`}>
-						<SetSize 
+						<SetSize
 							path={path}
 							fonts={fonts}
 							num={num}
@@ -52,52 +52,62 @@ const HomePage = () => {
 				</Switch>
 			</section>
 		</article>
-		);
+	);
 }
 
 //Here is the start pairing content.
 //If this is the flow we want to go for. The flow is accesable from the home screen. 
 
-const PairFont = ({path,fonts,setFont}) => {
+const PairFont = ({ path, fonts, setFont }) => {
 	//Here insert jason file with all of our fonts.
 	let fontlist = ['Arial', 'Arial Black', 'Tahoma', 'Verdana', 'Sans-Serif', 'Times New Roman', 'Cambria', 'Georgia', 'Serif'];
 
 	const changeFontOne = (e) => {
 		e.preventDefault();
-		setFont({first:e.target.value});
+		setFont({ first: e.target.value });
 	}
 
 	const changeFontTwo = (e) => {
 		e.preventDefault();
-		setFont({second:e.target.value});
+		setFont({ second: e.target.value });
 	}
 	return (
 		<section className="grid">
-			<div className="bg-dark col--6">
-				<div className="p-xs-bs"><Link to={`${path}/1`}>Next</Link></div>
-				<section className="grid">
-					<input className="font-input m-xs-bm col--6" type="text" value={fonts.first} onChange={changeFontOne} />
-					<input className="font-input p-xs-s m-xs-bm offset--7 col--6" type="text" value={fonts.second} onChange={changeFontTwo} />
-				</section>
-				<ClickList 
-					data={fontlist} 
+			<div className="selection bg-dark col--6">
+				<div className="selection-container">
+					<div className="nav-selection-links">
+						<div className="link-setsize p-xs-bs"><Link to={`${path}/1`}>FontSize</Link></div>
+					</div>
+					<div className="font-input-container grid">
+						<div className="font-input-name flex-xs-parent col--12">
+							<p className="font-input-header col--6">Header</p>
+							<p className="font-input-body offset--7 col--6">Body</p>
+						</div>
+						<div className="font-input-box flex-xs-parent w-100 col--12">
+							<input className="font-input col--6" type="text" value={fonts.first} onChange={changeFontOne} />
+							<input className="font-input offset--7 col--6" type="text" value={fonts.second} onChange={changeFontTwo} />
+						</div>
+					</div>
+				</div>
+				<ClickList
+					data={fontlist}
 					callback={setFont}
-					/>
+				/>
 			</div>
-			<div className="col--6 offset--7 pairing-wrapper">
-				<PairingDoc 
-					fontSize={72} 
-					fontStyleTwo={fonts.second} 
-					fontStyleOne={fonts.first} 
+			<div className="preview col--6 offset--7 pairing-wrapper">
+				<PairingDoc
+					fontSize={72}
+					fontStyleTwo={fonts.second}
+					fontStyleOne={fonts.first}
 					fontNameOne={fonts.first}
 					fontNameTwo={fonts.second}
-					/>
+				/>
 			</div>
 		</section>
 	);
 }
 
-const SetSize = ({path,fonts,num,setNum}) => {
+const SetSize = ({ path, fonts, num, setNum }) => {
 
 	const doNum = e => {
 		e.preventDefault();
@@ -108,16 +118,16 @@ const SetSize = ({path,fonts,num,setNum}) => {
 		<section className="grid">
 			<div className="col--6">
 				<div className="p-xs-bs"><Link to={`${path}/2`}>Next</Link></div>
-				<input type="range" value={num} min="12" max="72" id="font-size" onChange={doNum}/>
+				<input type="range" value={num} min="12" max="72" id="font-size" onChange={doNum} />
 			</div>
 			<div className="col--6 offset--7 pairing-wrapper">
-				<PairingDoc 
-					fontSize={num} 
-					fontStyleTwo={fonts.second} 
-					fontStyleOne={fonts.first} 
+				<PairingDoc
+					fontSize={num}
+					fontStyleTwo={fonts.second}
+					fontStyleOne={fonts.first}
 					fontNameOne={fonts.first}
 					fontNameTwo={fonts.second}
-					/>
+				/>
 			</div>
 		</section>
 	);
