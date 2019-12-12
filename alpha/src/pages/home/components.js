9d0a905947543443ca269c71767ad5c295dd3b4d
Raymond Shen <Raymonds.shenz@gmail.com>
2019-12-11 23:07:28 -0800

    Updated Styling Dec 11

--- alpha/src/pages/home/components.js
+++ alpha/src/pages/home/components.js
@@ -100,20 +100,20 @@ const FontRange = ({id,size,setSize}) => {
 
 const Spacing = ({id,spacing,setTracking, setLeading}) => {
 	return (
-  <div id="selectfont" className="fontsize-slider-container">
+  <div className=" spacingfont fontsize-slider-container">
 
-      <h5 className="txt-purewhite fontsize-head spacing-width-100">{spacing[id].name}</h5>
+      <h5 className="txt-purewhite spacing-width-100">{spacing[id].name}</h5>
       <br/>
-      <div className="spacing-width grid">
-        <span className="txt-purewhite fontsize-head col--2">{spacing[id].label2}</span>
+      <div className="spacing-lead spacing-width flex-row">
+        <span className="txt-purewhite spacing-head-label">{spacing[id].label2}</span>
         <input className="fontsize-slider-track fontsize-slider-track-limited-width col--8 m-xs-ts" type="range" value={spacing[id].leadingSize} min={spacing[id].leadingMin} max={spacing[id].leadingMax} onChange={setLeading} data-id={id}/>
-        <input className="fontsize-slider-input fontsize-slider-track-limited-width" type="number" value={spacing[id].leadingSize} min={spacing[id].leadingMin} max={spacing[id].leadingMax} onChange={setLeading} data-id={id}/>
+        <input className="fontsize-slider-input spacing-input" type="number" value={spacing[id].leadingSize} min={spacing[id].leadingMin} max={spacing[id].leadingMax} onChange={setLeading} data-id={id}/>
       </div>
       <br/>
-      <div className="gird spacing-width">
-        <span className="txt-purewhite fontsize-head col--2">{spacing[id].label1}</span>
+      <div className="spacing-track spacing-width flex-row">
+        <span className="txt-purewhite spacing-head-label">{spacing[id].label1}</span>
         <input className="fontsize-slider-track fontsize-slider-track-limited-width" type="range" value={spacing[id].trackingSize} min={spacing[id].trackingMin} max={spacing[id].trackingMax} onChange={setTracking} data-id={id}/>
-        <input className="fontsize-slider-input fontsize-slider-track-limited-width" type="number" value={spacing[id].trackingSize} min={spacing[id].trackingMin} max={spacing[id].trackingMax} onChange={setTracking} data-id={id}/>
+        <input className="fontsize-slider-input spacing-input" type="number" value={spacing[id].trackingSize} min={spacing[id].trackingMin} max={spacing[id].trackingMax} onChange={setTracking} data-id={id}/>
       </div>
 	</div>);
 }
