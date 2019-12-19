cd75162c42b5d6b8f57502e98d7f698c04ac4a84
Lovisa <elinlovisa.berglund@gmail.com>
2019-12-18 21:55:59 -0800

    Lovisa Commit before Merge

--- alpha/src/App.js
+++ alpha/src/App.js
@@ -16,9 +16,9 @@ import {
 //The main components are the ones that will be displayed at all times. 
 //Etc. the nav, the footer. The components that will be in the blocks that make up the pages can be imported directlly into the pages and then the pages into the main app.
 import {HeaderNav} from "./components/nav";
-
 import {PairingDoc} from "./components/pairing-doc";
 import { fontFamiliesData, fontSizesData, spacingsData } from "./pairingdoc-data";
+import LogoOutline from "./assets/images/logo_outline.svg";
 
 //Here goes the pages that we're importing to the main app.
 //The pages are the contet that will be exchanged in the app.
@@ -94,12 +94,17 @@ const IntroPage = (fonts) => {
 
   return (<div className="bg-dark">
     <section className="landingpage grid">
-      <div className="container max-xs-s p-xs-txl col--6">
+      <div className="container max-xs-s p-xs-txl col-md-6 col-xs-10">
         <h1 className="txt-purewhite">Experiment with font pairing and styling options while seeing the results live</h1>
-        <p className="txt-purewhite">Choose a body and header font to pair, and then customize each with styling options. Once you are complete with the process you will have the option to download a personal style-guide.</p>
-        <Link className="btn btn--secondary" to="pair-fonts">Get Started</Link>
+        <section className="grid">
+          <p className="txt-purewhite col-md-6 col-xs-10 offset-xs-0 offset-md-6">Choose a body and header font to pair, and then customize each with styling options. Once you are complete with the process you will have the option to download a personal style-guide.</p>
+          <Link className="btn btn--secondary col-xs-10 offset-xs-0 offset-md-6" to="pair-fonts">Get Started</Link>
+        </section>
+        <section>
+        <img className="logo-outline" src={LogoOutline} />
+        </section>
       </div>
-      <div className="container max-xs-s p-xs-txl col--6">
+      <div className="container max-xs-s p-xs-txl col--6 display-xs-n display-md-b">
         <PairingDoc 
 					fontSizes={fontSizesData} 
 					fontFamilies={fontFamilies}
