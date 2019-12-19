34dd565d33316526e024e7aff7c4ef94a2a05a68
Gustav <gustav.bodin1@gmail.com>
2019-12-19 11:29:04 -0800

    Merge push

--- alpha/src/App.js
+++ alpha/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState,useEffect,useReducer} from 'react';
+import React, {useState,useEffect} from 'react';
 import './styles/App.css';
 import './styles/style.min.css';
 import {
---
+++ alpha/src/pages/intro.js
@@ -0,0 +1,50 @@
+import React, { useReducer, useEffect } from 'react';
+import {PairingDoc} from "../components/pairing-doc";
+import { fontFamiliesData, fontSizesData, spacingsData } from "../pairingdoc-data";
+import LogoOutline from "../assets/images/logo_outline.svg";
+
+import {Link} from 'react-router-dom';
+
+const IntroPage = (fonts) => {
+    const reduceIndex = (s, a) => ({ ...s, ...a });
+    
+    let [fontFamilies, setFontFamilies] = useReducer(
+          reduceIndex,
+          fontFamiliesData
+    );
+  
+    useEffect(() => {
+          if (!fonts.length) return;
+          let r1 = Math.floor(Math.random() * fonts.length);
+          let r2 = Math.floor(Math.random() * fonts.filter((o, i) => i !== r1).length);
+          console.log(r1, r2, fonts[r1], fonts[r2])
+          setFontFamilies({ first: fonts[r1] })
+          setFontFamilies({ second: fonts[r2] })
+    }, [fonts]);
+      
+    return (<div className="bg-dark">
+      <section className="landingpage grid">
+        <div className="container max-xs-s p-xs-txl col-md-6 col-xs-10">
+          <h1 className="txt-purewhite">Experiment with font pairing and styling options while seeing the results live</h1>
+          <section className="grid">
+            <p className="txt-purewhite col-md-6 col-xs-10 offset-xs-0 offset-md-6">Choose a body and header font to pair, and then customize each with styling options. Once you are complete with the process you will have the option to download a personal style-guide.</p>
+            <Link className="btn btn--secondary col-xs-10 offset-xs-0 offset-md-6" to="pair-fonts">Get Started</Link>
+          </section>
+          <section>
+          <img className="logo-outline" src={LogoOutline} />
+          </section>
+        </div>
+        <div className="container max-xs-s p-xs-txl col--6 display-xs-n display-md-b">
+          <PairingDoc 
+                      fontSizes={fontSizesData} 
+                      fontFamilies={fontFamilies}
+            spacings={spacingsData}
+            className='landingpage__pairingdoc'
+                      />
+        </div>
+      </section>
+  
+    </div>);
+  };
+
+  export default IntroPage;
